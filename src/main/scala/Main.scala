0479502a6fd653cda15a560c3e9709b6a075a850
Bartosz Gajda <bg@bartoszgajda.com>
2019-06-22 14:40:44 +0200

    Scala lesson two.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -26,6 +26,37 @@ object Main {
 
     // String printing tricks
     println("The mess" + numberOne + truth + letterA + pi + bigNumber)
-    println(f"Pi is about $$")
+    println(f"Pi is about $piSinglePrecision%.3f")
+    println(f"Zero paddin on the left: $numberOne%05d")
+
+    // Substituting in variables
+    println(s"I can use the s prefix to use variables like $numberOne $truth $letterA")
+
+    // Substituting expressions (with curly brackets)
+    println(s"The s prefix isn't limited to variables like ${1+2}")
+
+    // Using regular expressions
+    val theUltimateAnswer: String = "To life, the universe, and everything is 42."
+    val pattern = """.* ([\d]+).""".r
+    val pattern(answerString) = theUltimateAnswer
+    val answer = answerString.toInt
+    println(answer)
+
+    // Dealing with booleans
+    val isGreater = 1 > 2
+    val isLesser = 1 < 2
+    val impossible = isGreater & isLesser // Better safety
+    val anotherWay = isGreater && isLesser // Bail out earlier
+
+    val picard: String = "Picard"
+    val bestCaptain: String = "Picard"
+    val isBest: Boolean = picard == bestCaptain
+
+    // EXERCISE
+    // Write some code that takes the value of pi, doubles it and then prints it withing a string with
+    // three decimal places of precision to the right
+    val secondPi: Float = 3.14159265f
+    val doubledPi = secondPi * 2
+    println(f"The doubled Pi equals: $doubledPi%.3f")
   }
 }
